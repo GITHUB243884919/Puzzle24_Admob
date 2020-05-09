b2305bcbb57113470997577d7f56f3eb2eb7df78
243884919 <243884919@QQ.COM>
2020-05-09 20:28:56 +0800

    Init

---
+++ 24Point-admob/Assets/Scripts/Game/GameDefine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// 运算操作
+    /// </summary>
+    public enum CalcOper
+    {
+
+        None,
+        Add,
+        Sub,
+        Mul,
+        Div,
+    }
+
+    public class GameConst
+    {
+        public static int Default_LeftNumResetPoker = 10;
+        public static int Default_LeftNumTips = 5;
+    }
+
+}
---
+++ 24Point-admob/Assets/Scripts/Game/UI/UIResult.cs
@@ -0,0 +1,32 @@
+﻿using Game.MessageCenter;
+using UFrame;
+using UnityEngine;
+
+public class UIResult : UIPage
+{
+    public UIResult() : base(UIType.PopUp, UIMode.DoNothing, UICollider.None)
+    {
+        uiPath = "UIPrefab/UIResult";
+    }
+    public override void Awake(GameObject go)
+    {
+        base.Awake(go);
+        //初始化控件
+        RegComponent();
+        GetTransPrefabAllTextShow(this.transform);
+    }
+
+    protected void RegComponent()
+    {
+        RegistBtnAndClick("oper_grid/op_next", OnClickedNextLevel);
+    }
+
+    protected void OnClickedNextLevel(string objName)
+    {
+        PageMgr.ClosePage(this);
+        var level = PageMgr.GetPage<UILevel>();
+        level.GenLevel(true);
+    }
+
+}
+
